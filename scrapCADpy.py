6dfd6d7bd36f02e29354bff46a2e78cb1cb8e936
AM4SO <79862724+AM4SO@users.noreply.github.com>
2022-11-16 14:26:41 +0000

    update

--- scrapCADpy.py
+++ scrapCADpy.py
@@ -1,4 +1,19 @@
 from pyStl import Vector, Solid, Triangle
+from argparse import ArgumentParser
+import os
+import json
+
+parser = ArgumentParser()
+parser.add_argument("-f", "--file", dest="filePath", help="scrap mechanic model JSON file")
+parser.add_argument("-n", "--name", dest="modelName", help="Name of model (will be used in when saving the generated STL)")
+args = parser.parse_args()
+path = ""
+modelName = "testThing"
+
+if args.filePath and os.path.exists(args.filePath):
+    path = args.filePath
+if args.modelName:
+    modelName = args.modelName
 
 rludbf = (Vector.right, -Vector.right, Vector.up, -Vector.up, Vector.back, -Vector.back)
 
@@ -58,7 +73,7 @@ class Blueprint:
         for part in self.blueprintObj:
             position = part["pos"] - self.minPos
             bounds = Vector(1,1,1)
-            if part["bounds"]:
+            if part.get("bounds"):
                 bounds = part["bounds"]
             endPos = position + bounds - Vector.ones
             for x in range(bounds.x):
@@ -81,9 +96,9 @@ class Blueprint:
                     l = x==0 or blockArray[x-1][y][z] == "0"
                     r = x==len(blockArray)-1 or blockArray[x+1][y][z] == "0"
                     d = z==0 or blockArray[x][y][z-1] == "0"
-                    u = z==len(blockArray)-1 or blockArray[x][y][z+1] == "0"
+                    u = z==len(blockArray[x][y])-1 or blockArray[x][y][z+1] == "0"
                     f = y==0 or blockArray[x][y-1][z] == "0"
-                    b = y==len(blockArray)-1 or blockArray[x][y+1][z] == "0"
+                    b = y==len(blockArray[x])-1 or blockArray[x][y+1][z] == "0"
                     self.solid.createCube(Vector(x,y,z),1, includelrdufb=(l,r,d,u,f,b))
         
     def calcSize(self):
@@ -92,8 +107,10 @@ class Blueprint:
         minPos = Vector(1000000000, 1000000000, 1000000000)
         for part in self.blueprintObj:
             minPos = minPos.minPerAxis(part["pos"])
-            if part["bounds"]:
-                maxPos = maxPos.maxPerAxis(part["pos"]+part["bounds"])# Only tells us number of blocks in each direction
+            bounds = Vector.ones
+            if part.get("bounds"):
+                bounds = part["bounds"]
+            maxPos = maxPos.maxPerAxis(part["pos"]+bounds)# Only tells us number of blocks in each direction
         self.minPos = minPos
         self.maxPos = maxPos - Vector.ones ## -1 to get coords instead of blocks in each direction
         self.objBounds = maxPos - minPos ## tells us array size
@@ -105,7 +122,7 @@ class Blueprint:
         for body in blueprint["bodies"]:
             for part in body["childs"]:
                 part["pos"] = Vector(part["pos"]["x"], part["pos"]["y"], part["pos"]["z"])
-                if part["bounds"]:
+                if part.get("bounds"):
                     part["bounds"] = Vector(part["bounds"]["x"], part["bounds"]["y"], part["bounds"]["z"])
                 self.blueprintObj.append(part)
 
@@ -118,5 +135,9 @@ in it. This is a 3d array which holds the blocks in it. Blocks which are larger 
 can have the center of the block stored in the correct location, and the rest of the block
 not need to be stored.
 '''
-x={"bodies":[{"childs":[{"bounds":{"x":4,"y":1,"z":4},"color":"9B683A","pos":{"x":-7,"y":8,"z":2},"shapeId":"df953d9c-234f-4ac2-af5e-f0490b223e71","xaxis":1,"zaxis":3},{"bounds":{"x":1,"y":3,"z":1},"color":"9B683A","pos":{"x":-5,"y":5,"z":3},"shapeId":"df953d9c-234f-4ac2-af5e-f0490b223e71","xaxis":1,"zaxis":3}]}],"version":4}
-x = Blueprint(x, "SomethingyThingTestThing")
+file = open(path)
+x = file.read()
+file.close()
+x = json.loads(x)
+#x={"bodies":[{"childs":[{"bounds":{"x":4,"y":1,"z":4},"color":"9B683A","pos":{"x":-7,"y":8,"z":2},"shapeId":"df953d9c-234f-4ac2-af5e-f0490b223e71","xaxis":1,"zaxis":3},{"bounds":{"x":1,"y":3,"z":1},"color":"9B683A","pos":{"x":-5,"y":5,"z":3},"shapeId":"df953d9c-234f-4ac2-af5e-f0490b223e71","xaxis":1,"zaxis":3}]}],"version":4}
+x = Blueprint(x, modelName)
